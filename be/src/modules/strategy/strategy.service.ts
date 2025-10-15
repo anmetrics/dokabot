c42127a66ea1266511d9018456f24c875346ae1e
thien <tranquocthien@aol.com>
2025-10-15 13:13:55 +0700

    Update

--- be/src/modules/strategy/strategy.service.ts
+++ be/src/modules/strategy/strategy.service.ts
@@ -1,37 +1,37 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { BinanceService } from '../binance/binance.service';
-import { IStrategy } from './strategies/strategy.interface';
+import { IStrategy } from './strategy.interface';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { RsiReversalDcaStrategy } from './strategies/rsi-reversal.strategy';
 
 @Injectable()
 export class StrategyService {
   private logger = new Logger('StrategyService');
-  private strategy: IStrategy;
+  private strategy1: IStrategy;
+  private strategy2: IStrategy;
 
   constructor(
     private readonly binanceService: BinanceService,
     private readonly eventEmitter: EventEmitter2,
   ) {}
 
-  async startStrategy(name: string) {
-    const symbol = (process.env.SYMBOL || 'BTCUSDT').toUpperCase();
+  startStrategy(name: string) {
+    this.strategy1 = new RsiReversalDcaStrategy(
+      this.binanceService,
+      'BTCUSDT',
+      50,
+      '5m',
+    );
+    this.strategy2 = new RsiReversalDcaStrategy(
+      this.binanceService,
+      'BNBUSDT',
+      100,
+      '3m',
+    );
 
-    if (name === 'ema-macd') {
-      this.strategy = new RsiReversalDcaStrategy(this.binanceService, symbol);
-    } else {
-      throw new Error('Strategy not found');
-    }
-
-    await this.strategy.startAll();
+    this.strategy1.startAll();
+    this.strategy2.startAll();
 
     this.logger.log(`Started strategy: ${name}`);
-  }
-
-  stopStrategy() {
-    if (this.strategy) {
-      this.strategy.stop();
-      this.logger.log('Stopped strategy');
-    }
   }
 }
